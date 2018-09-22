2bbbc91be774df5d4a3829ce097709bacf9c6a6a
rifkegribenes <rifkegribenes@gmail.com>
2018-09-21 17:38:08 -0700

    fix scales

--- script.js
+++ script.js
@@ -8,57 +8,63 @@ d3.select("body")
   .append("div")
   .attr("id", "tooltip");
 
-// initialize scales
-const x = d3.scaleLinear()
-    .domain([2.6, 75.1])
-    .rangeRound([600, 860]);
-
-const color = d3.scaleThreshold()
-    .domain(d3.range(2.6, 75.1, (75.1-2.6)/8))
-    .range(d3.schemeYlGnBu[9]);
-
-// initialize legend
-const g = svg.append("g")
-    .attr("class", "key")
-    .attr("id", "legend")
-    .attr("transform", "translate(0,40)");
-
-// add color blocks
-g.selectAll("rect")
-  .data(color.range().map((d) => {
-      d = color.invertExtent(d);
-      if (d[0] == null) d[0] = x.domain()[0];
-      if (d[1] == null) d[1] = x.domain()[1];
-      return d;
-    }))
-  .enter().append("rect")
-    .attr("height", 8)
-    .attr("x", (d) =>  x(d[0]))
-    .attr("width", (d) => x(d[1]) - x(d[0]))
-    .attr("fill", (d) => color(d[0]));
-
-// add labels
-g.append("text")
-    .attr("x", x.range()[0])
-    .attr("y", -6)
-    .attr("fill", "#000")
-    .attr("text-anchor", "start")
-    .attr("font-weight", "bold")
-    .text("Educational attainment");
-
-// add ticks
-g.call(d3.axisBottom(x)
-    .tickSize(13)
-    .tickFormat((x) => `${Math.round(x)}%`)
-    .tickValues(color.domain()))
-    .select(".domain")
-    .remove();
-
 // render map
 const render = (error, us, education) => {
   if (error) throw error;
 
-  const edArray = education.map(county => county.bachelorsOrHigher);
+  // find min and max values
+  const edu = {
+    min: d3.min(education, d => d.bachelorsOrHigher),
+    max: d3.max(education, d => d.bachelorsOrHigher),
+  };
+
+  edu.range = d3.range(edu.min, edu.max, (edu.max - edu.min) / 8);
+
+  // initialize scales
+  const x = d3.scaleLinear()
+      .domain([edu.min, edu.max])
+      .rangeRound([600, 860]);
+
+  const color = d3.scaleThreshold()
+      .domain(edu.range)
+      .range(d3.schemeYlGnBu[9]);
+
+  // initialize legend
+  const g = svg.append("g")
+      .attr("class", "key")
+      .attr("id", "legend")
+      .attr("transform", "translate(0,40)");
+
+  // add color blocks
+  g.selectAll("rect")
+    .data(color.range().map((d) => {
+        d = color.invertExtent(d);
+        if (d[0] == null) d[0] = x.domain()[0];
+        if (d[1] == null) d[1] = x.domain()[1];
+        return d;
+      }))
+    .enter().append("rect")
+      .attr("height", 8)
+      .attr("x", (d) =>  x(d[0]))
+      .attr("width", (d) => x(d[1]) - x(d[0]))
+      .attr("fill", (d) => color(d[0]));
+
+  // add labels
+  g.append("text")
+      .attr("x", x.range()[0])
+      .attr("y", -6)
+      .attr("fill", "#000")
+      .attr("text-anchor", "start")
+      .attr("font-weight", "bold")
+      .text("Educational attainment");
+
+  // add ticks
+  g.call(d3.axisBottom(x)
+      .tickSize(13)
+      .tickFormat((x) => `${Math.round(x)}%`)
+      .tickValues(color.domain()))
+      .select(".domain")
+      .remove();
 
   // find matching county data from education array
   const countyDataCallback = (d, col) => {
